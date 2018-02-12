7ff4fe4f57206e0545005e4729467d012ff5bc36
Artem_Ananchenko <artem_ananchenko@epam.com>
2018-02-12 20:29:26 +0300

    Done part 1

--- src/main/java/spliterators/part1/exercise/RectangleSpliterator.java
+++ src/main/java/spliterators/part1/exercise/RectangleSpliterator.java
@@ -6,12 +6,11 @@ import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
-
-    private final int innerLength;
     private final int[][] array;
-    private final int startOuterInclusive;
+    private final int innerLength;
+    private int startOuterInclusive;
     private final int endOuterExclusive;
-    private final int startInnerInclusive;
+    private int startInnerInclusive;
 
     public RectangleSpliterator(int[][] array) {
         this(array, 0, array.length, 0);
@@ -20,8 +19,8 @@ public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
     private RectangleSpliterator(int[][] array, int startOuterInclusive, int endOuterExclusive, int startInnerInclusive) {
         super(Long.MAX_VALUE, Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.SIZED | Spliterator.SUBSIZED | Spliterator.NONNULL);
 
-        innerLength = array.length == 0 ? 0 : array[0].length;
         this.array = array;
+        this.innerLength = array.length == 0 ? 0 : array[0].length;
         this.startOuterInclusive = startOuterInclusive;
         this.endOuterExclusive = endOuterExclusive;
         this.startInnerInclusive = startInnerInclusive;
@@ -29,18 +28,36 @@ public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
     @Override
     public RectangleSpliterator trySplit() {
-        // TODO
-        throw new UnsupportedOperationException();
+        RectangleSpliterator newSpliterator = null;
+
+        if (endOuterExclusive - startOuterInclusive > 1) {
+            int midOuter = (endOuterExclusive + startOuterInclusive) >> 1;
+            newSpliterator = new RectangleSpliterator(array, startOuterInclusive, midOuter,
+                    0);
+            startOuterInclusive = midOuter;
+        }
+
+        return newSpliterator;
     }
 
     @Override
     public long estimateSize() {
-        return ((long) endOuterExclusive - startOuterInclusive)*innerLength - startInnerInclusive;
+        return ((long) endOuterExclusive - startOuterInclusive) * innerLength - startInnerInclusive;
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        if (startInnerInclusive >= innerLength){
+            startInnerInclusive = 0;
+            startOuterInclusive++;
+        }
+        if (startOuterInclusive >= endOuterExclusive) {
+            return false;
+        }
+
+        action.accept(array[startOuterInclusive][startInnerInclusive++]);
+
+
+        return true;
     }
 }
