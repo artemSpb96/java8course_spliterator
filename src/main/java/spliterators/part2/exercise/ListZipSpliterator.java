73bef471876a1025e9290ca75a5acd0440103273
Artem_Ananchenko <artem_ananchenko@epam.com>
2018-02-13 12:12:35 +0300

    Done part 2

--- src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
+++ src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
@@ -6,31 +6,59 @@ import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ListZipSpliterator<L, R, T>  implements Spliterator<T> {
-    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
 
+
+    private List<L> list1;
+    private List<R> list2;
+    private BiFunction<L, R, T> combiner;
+
+    private int startInclusive;
+    private int endExclusive;
+
+    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.combiner = combiner;
+        this.startInclusive = 0;
+        this.endExclusive = Math.min(list1.size(), list2.size());
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
-        //TODO
-        throw new UnsupportedOperationException();
+        if (startInclusive >= endExclusive) {
+            return false;
+        }
+
+        action.accept(combiner.apply(list1.get(startInclusive), list2.get(startInclusive)));
+        startInclusive++;
+
+        return true;
     }
 
     @Override
     public Spliterator<T> trySplit() {
-        //TODO
-        throw new UnsupportedOperationException();
+        if (endExclusive - startInclusive < 2) {
+            return null;
+        }
+
+        int mid = (startInclusive + endExclusive) >> 1;
+
+        ListZipSpliterator<L, R, T> newSpliterator = new ListZipSpliterator<>(list1.subList(startInclusive, mid),
+                list2.subList(startInclusive, mid), combiner);
+
+        startInclusive = mid;
+
+        return newSpliterator;
     }
 
     @Override
     public long estimateSize() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return endExclusive - startInclusive;
     }
 
     @Override
     public int characteristics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.SIZED | Spliterator.SUBSIZED |
+                Spliterator.NONNULL;
     }
 }
